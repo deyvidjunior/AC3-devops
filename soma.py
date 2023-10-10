e104582d9eaf616caa4fbdd5fdb6f3acaa8e3923
deyvidjunior <142635514+deyvidjunior@users.noreply.github.com>
2023-10-09 21:05:57 -0300

    Create soma.py
    somando numeros.

---
+++ soma.py
@@ -0,0 +1,9 @@
+# Solicita que o usuário insira dois números
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+
+# Realiza a soma dos números
+soma = numero1 + numero2
+
+# Exibe o resultado
+print("A soma de", numero1, "e", numero2, "é igual a", soma)
