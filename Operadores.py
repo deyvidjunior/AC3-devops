f1a4fb93429ace5c568ac8d323483cec419b8c0e
MLBispo <143048508+MLBispo@users.noreply.github.com>
2023-10-14 17:49:36 -0300

    Update Operadores.py

--- Operadores.py
+++ Operadores.py
@@ -4,7 +4,7 @@ adicao = numero1 + numero2
 subtracao = numero1 - numero2
 multiplicacao = numero1 * numero2
 divisao = numero1 / numero2
-print('Adição: ', adicao)
-print('Subtração: ', subtracao)
-print('Multiplicação: ', multiplicacao)
-print('Divisão: ', round(divisao, 2))
+print('Adição: ', adiccao)
+print('Subtração: ', subtraccao)
+print('Multiplicação: ', multipliccacao)
+print('Divisão: ', round(divisaao, 2))
