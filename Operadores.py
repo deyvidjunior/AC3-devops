c0c628a03799bc845f83ba83a95ac831e3bf178e
MLBispo <143048508+MLBispo@users.noreply.github.com>
2023-10-14 19:15:33 -0300

    Operadores.py

--- Operadores.py
+++ Operadores.py
@@ -1,10 +1,10 @@
 numero1 = 5
 numero2 = 5
-adicao = numero3 + numero4
-subtracao = numero3 - numero4
-multiplicacao = numero3 * numero4
-divisao = numero3 / numero4
-print;('Adição: ', adicao)
-print;('Subtração: ', subtracao)
-print;('Multiplicação: ',multiplicacao)
-print;('Divisão: ',round(divisao, 2))
+adicao = numero1 + numero2
+subtracao = numero1 - numero2
+multiplicacao = numero1 * numero2
+divisao = numero1 / numero2
+print('Adição: ', adicao)
+print('Subtração: ', subtracao)
+print('Multiplicação: ',multiplicacao)
+print('Divisão: ',round(divisao, 2))
