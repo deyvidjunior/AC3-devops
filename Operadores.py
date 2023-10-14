b4d6e3d7002c7dba5eb8ebb4730c688e840e8f84
MLBispo <143048508+MLBispo@users.noreply.github.com>
2023-10-14 17:51:43 -0300

    Update Operadores.py

--- Operadores.py
+++ Operadores.py
@@ -1,10 +1,10 @@
 numero1 = int(input('Digite o primeiro número: '))
-numero3 = int(input('Digite o segundo número: '))
+numero2 = int(input('Digite o segundo número: '))
 adicao = numero1 + numero2
 subtracao = numero1 - numero2
 multiplicacao = numero1 * numero2
 divisao = numero1 / numero2
-print('Adição: ', adiccao)
-print('Subtração: ', subtraccao)
-print('Multiplicação: ', multipliccacao)
-print('Divisão: ', round(divisaao, 2))
+print('Adição: ', adicao)
+print('Subtração: ', subtracao)
+print('Multiplicação: ', multiplicacao)
+print('Divisão: ', round(divisao, 2))
